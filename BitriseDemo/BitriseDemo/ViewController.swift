5d01a50df6103b31f810f71d69aaa13400051ec7
Sergey Nikolaev <sergey.nikolaev@axondevgroup.com>
2018-09-11 11:30:28 +0300

    Initial commit.

---
+++ BitriseDemo/BitriseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Sergey Nikolaev on 9/11/18.
+//  Copyright © 2018 Axon Development Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
